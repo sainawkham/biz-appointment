7d50229c942d27633d74680c5e64568f6fdde148
sainawkham <sainawkham@gmail.com>
2021-07-25 19:28:23 +0630

    2 files modified

--- src/app/companies/companies.page.ts
+++ src/app/companies/companies.page.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-companies',
   templateUrl: './companies.page.html',
-  styleUrls: ['./companies.page.scss'],
+  styleUrls: ['./companies.page.scss']
 })
 export class CompaniesPage implements OnInit {
-
-  constructor() { }
+  constructor(public db: AngularFirestore) {}
 
   ngOnInit() {
+   
   }
-
 }
