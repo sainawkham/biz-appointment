c984960f6aea67ed70e04719857cd099aa5d67f4
sainawkham <sainawkham@gmail.com>
2021-07-29 10:16:55 +0630

    2 files modified

--- src/app/companies/companies.page.ts
+++ src/app/companies/companies.page.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, NgZone } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../Shared/authentication-service';
 import { CompaniesService } from '../services/companies.service';
 
 export class Companies {
@@ -18,9 +19,23 @@ export class CompaniesPage implements OnInit {
 
   comList : Companies[];
   
-  constructor(private comService: CompaniesService) {}
+  constructor(private comService: CompaniesService,
+              public authService: AuthenticationService,
+              public ngZone: NgZone,
+              public router: Router) {}
 
   ngOnInit() {
+    //check login or not. if not login goto login page.
+    this.authService.ngFireAuth.onAuthStateChanged(user => {
+      if (user) {
+        //console.log('user is logged in, info: ', user);
+      } else {
+        this.ngZone.run(() => {
+          this.router.navigate(['login']);
+        })
+      }
+    });
+    // read from firestore
    this.comService.getCompanies().subscribe((res)=> {
      this.comList = res.map((t) => {
        return {
